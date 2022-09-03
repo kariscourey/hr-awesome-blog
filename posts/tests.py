f1894a0248d8b674b399c9862fccb65427c0f949
karis courey <kariscourey@gmail.com>
2022-09-02 22:51:46 -0400

    update blog with tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client  # client allows to make fake requests to views
+from django.utils import timezone
+from posts.models import Post
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+class PostModelTestCase(TestCase):
+    def test_title_should_be_less_than_100(self):
+        # Arrange
+        # Create sample data for post
+        title = "X" * 200
+        created_at = timezone.now()
+        content = "whatever"
+
+        # Act
+        post = Post.objects.create(title=title,created_at=created_at,content=content)
+
+        # Create post
+        # Assert
+        # assert it failed
+        self.assertNotEqual(post.title,title)
+
+    def test_created_at_is_a_required_field(self):
+        title = "Sample Post"
+        content = "Sample Content"
+
+        try:
+            post = Post.objects.create(title=title,content=content)
+        except IntegrityError as error:
+            self.assertIsInstance(error, IntegrityError)
+
+        with self.assertRaises():
+            post = Post.objects.create(title=title, content=content)
+
+class PostListViewTestCase(TestCase):
+    pass
